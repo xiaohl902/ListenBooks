bfeca03d43a539fbb69bafc11528f7e0507b7b76
xiaohl_902 <1205427587@qq.com>
2016-10-27 20:21:30 +0800

    欢迎页延迟跳转

--- app/src/main/java/com/mwkj/util/Constant.java
+++ app/src/main/java/com/mwkj/util/Constant.java
@@ -5,14 +5,17 @@ public interface Constant {
 
     String HOME = "http://www.mow99.com/";
 
+    //用户登录资料，欢迎页图片,更新的apk地址
+    String USER_WELCOME = "MOWLoadingV11?userId=141610&channelNumber=0&appVersion=1.7&session=&SecurityID=mow99_2014_12_30_@~$;&clientVersion=170&OS=1&imei=864394010755355";
+
     //听书馆
-   String STORYTELLING =  "PSIndex2?session=1416101477468719685&SecurityID=mow99_2014_12_30_@~$;&clientVersion=170&OS=1&imei=864394010755355";
+    String STORYTELLING =  "PSIndex2?session=1416101477468719685&SecurityID=mow99_2014_12_30_@~$;&clientVersion=170&OS=1&imei=864394010755355";
 
     //相声
-   String CROSSTALK =  "XSIndex2?session=1416101477468719685&SecurityID=mow99_2014_12_30_@~$;&clientVersion=170&OS=1&imei=864394010755355";
+    String CROSSTALK =  "XSIndex2?session=1416101477468719685&SecurityID=mow99_2014_12_30_@~$;&clientVersion=170&OS=1&imei=864394010755355";
 
     //专题
-   String SPECIAL =  "subjectIndex?pageSize=8&pageNumber=1&session=1416101477468719685&SecurityID=mow99_2014_12_30_@~$;&clientVersion=170&OS=1&imei=864394010755355";
+    String SPECIAL =  "subjectIndex?pageSize=8&pageNumber=1&session=1416101477468719685&SecurityID=mow99_2014_12_30_@~$;&clientVersion=170&OS=1&imei=864394010755355";
 
     //艺术家
   String ARTIST =   "ArtistIndex?pageSize=10&pageNumber=1&session=1416101477468719685&SecurityID=mow99_2014_12_30_@~$;&clientVersion=170&OS=1&imei=864394010755355";
--- app/src/main/java/com/mwkj/util/RetrofitService.java
+++ app/src/main/java/com/mwkj/util/RetrofitService.java
@@ -2,6 +2,7 @@ package com.mwkj.util;
 
 import com.mwkj.entity.CrosstalkEntity;
 
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
 
@@ -9,6 +10,11 @@ import retrofit2.http.GET;
  * Created by ${WU} on 2016/10/27.
  */
 public interface RetrofitService {
+
+    //欢迎页面 , 获取图片
+    @GET(Constant.USER_WELCOME)
+    Call<ResponseBody> getWelcomeDatas();
+
     @GET(Constant.CROSSTALK)
     Call<CrosstalkEntity> getCrosstalkEntityByUrl();
 }
