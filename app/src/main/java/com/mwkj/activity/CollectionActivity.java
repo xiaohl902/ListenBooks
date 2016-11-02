390a4b8b0fc8129f94811bb9edaf06fad1dabc6b
luckeystronglu <903804013@qq.com>
2016-11-02 16:47:00 +0800

    界面美化

--- app/src/main/java/com/mwkj/activity/CollectionActivity.java
+++ app/src/main/java/com/mwkj/activity/CollectionActivity.java
@@ -3,15 +3,23 @@ package com.mwkj.activity;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
+import android.view.ViewGroup;
 
 import com.mwkj.adapter.CollectionsAdapter;
 import com.mwkj.db.ArtistOpenHelper;
 import com.mwkj.entity.ArtistInfoEntity;
 import com.qf.kenlibrary.base.BaseActivity;
+import com.yanzhenjie.recyclerview.swipe.Closeable;
+import com.yanzhenjie.recyclerview.swipe.OnSwipeMenuItemClickListener;
+import com.yanzhenjie.recyclerview.swipe.SwipeMenu;
+import com.yanzhenjie.recyclerview.swipe.SwipeMenuCreator;
+import com.yanzhenjie.recyclerview.swipe.SwipeMenuItem;
+import com.yanzhenjie.recyclerview.swipe.SwipeMenuRecyclerView;
+import com.yanzhenjie.recyclerview.swipe.touch.OnItemMoveListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,10 +32,10 @@ import butterknife.OnClick;
  * Created by lenovo on 2016/11/1.
  * 浏览记录（收藏页）
  */
-public class CollectionActivity extends BaseActivity implements AdapterView.OnItemClickListener {
+public class CollectionActivity extends BaseActivity implements OnItemMoveListener {
 
-    @Bind(R.id.collect_lv)
-    ListView collectLv;
+    @Bind(R.id.collect_rv)
+    SwipeMenuRecyclerView collectRv;
 //    private ListView listView;
     private CollectionsAdapter collectionsAdapter;
     private ArtistOpenHelper helper;
@@ -43,14 +51,49 @@ public class CollectionActivity extends BaseActivity implements AdapterView.OnIt
 
     @Override
     protected void init() {
-//        artistname = findViewByIds(R.id.art_info_name);
-//        artistname.setText();
-        collectLv = findViewByIds(R.id.collect_lv);
-        collectLv.setOnItemClickListener(this);
+
         helper = new ArtistOpenHelper(this);
         database = helper.getReadableDatabase();
+//        artistname = findViewByIds(R.id.art_info_name);
+//        artistname.setText();
+//        collectRv = findViewByIds(R.id.collect_lv);
+        collectRv.setLayoutManager(new LinearLayoutManager(this));
         collectionsAdapter = new CollectionsAdapter(this);
-        collectLv.setAdapter(collectionsAdapter);
+        collectionsAdapter.setOnItemClickListener(new CollectionsAdapter.ArtInfoOnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                Intent intent = new Intent(CollectionActivity.this, ArtWorksActivity.class);
+                intent.putExtra("albumid", colist.get(position).getAlbumId());
+                intent.putExtra("chapternum", colist.get(position).getAlbumChapter());
+                intent.putExtra("fansnum", colist.get(position).getPlayNumber());
+                intent.putExtra("worksname", colist.get(position).getAlbumName());
+                intent.putExtra("worksimg", colist.get(position).getAlbumCover());
+                startActivity(intent);
+            }
+        });
+
+        collectRv.setAdapter(collectionsAdapter);
+        collectRv.setLongPressDragEnabled(true);//开启长按拖拽
+        collectRv.setItemViewSwipeEnabled(false);//开启滑动删除 ,与swipeMenuCreator只能用一种，否则会冲突
+        collectRv.setOnItemMoveListener(this);
+
+        collectRv.setSwipeMenuCreator(swipeMenuCreator);
+        collectRv.setSwipeMenuItemClickListener(new OnSwipeMenuItemClickListener() {
+            @Override
+            public void onItemClick(Closeable closeable, int adapterPosition, int menuPosition, int direction) {
+                closeable.smoothCloseMenu();//关闭被点击菜单
+                if (direction == SwipeMenuRecyclerView.RIGHT_DIRECTION){
+                    //右滑
+                    if (menuPosition == 0) {
+                        collectionsAdapter.moveItem(adapterPosition,0);
+                        collectRv.scrollToPosition(0);
+                    }else if (menuPosition == 1){
+                        collectionsAdapter.remove(adapterPosition);
+                    }
+                }
+            }
+        });
+
     }
 
     @Override
@@ -77,17 +120,6 @@ public class CollectionActivity extends BaseActivity implements AdapterView.OnIt
         cursor.close();
     }
 
-    //listview点击事件
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        Intent intent = new Intent(this, ArtWorksActivity.class);
-        intent.putExtra("albumid", colist.get(position).getAlbumId());
-        intent.putExtra("chapternum", colist.get(position).getAlbumChapter());
-        intent.putExtra("fansnum", colist.get(position).getPlayNumber());
-        intent.putExtra("worksname", colist.get(position).getAlbumName());
-        intent.putExtra("worksimg", colist.get(position).getAlbumCover());
-        startActivity(intent);
-    }
 
 
     @Override
@@ -107,4 +139,55 @@ public class CollectionActivity extends BaseActivity implements AdapterView.OnIt
                 break;
         }
     }
+
+    //setOnItemMoveListener的两个监听方法
+    @Override
+    public boolean onItemMove(int fromPosition, int toPosition) {
+        collectionsAdapter.moveItem(fromPosition,toPosition);
+        return true;
+    }
+
+    @Override
+    public void onItemDismiss(int position) {
+        collectionsAdapter.remove(position);
+    }
+
+    private SwipeMenuCreator swipeMenuCreator = new SwipeMenuCreator() {
+        @Override
+        public void onCreateMenu(SwipeMenu swipeLeftMenu, SwipeMenu swipeRightMenu, int viewType) {
+
+//            SwipeMenuItem addItem = new SwipeMenuItem(MainActivity.this)
+////                    .setBackgroundDrawable(R.mipmap.ic_launcher)// 点击的背景。
+//                    .setBackgroundDrawable(R.color.colorPrimary)// 点击的背景。
+//                    .setImage(android.R.drawable.ic_delete) // 图标。
+//                    .setWidth(200) // 宽度。
+//                    .setHeight(ViewGroup.LayoutParams.MATCH_PARENT); // 高度。
+//            swipeLeftMenu.addMenuItem(addItem); // 添加一个按钮到左侧菜单。
+
+            SwipeMenuItem deleteItem = new SwipeMenuItem(CollectionActivity.this)
+//                    .setBackgroundDrawable(new ColorDrawable(Color.YELLOW))
+                    .setImage(android.R.drawable.ic_dialog_map) // 图标。
+                    .setText("置顶") // 文字。
+                    .setTextColor(Color.RED) // 文字颜色。
+                    .setTextSize(16) // 文字大小。
+                    .setWidth(100)
+                    .setHeight(ViewGroup.LayoutParams.MATCH_PARENT);
+            swipeRightMenu.addMenuItem(deleteItem);// 添加一个按钮到右侧侧菜单。
+
+            SwipeMenuItem addItem2 = new SwipeMenuItem(CollectionActivity.this)
+//                    .setBackgroundDrawable(R.mipmap.ic_launcher)// 点击的背景。
+//                    .setBackgroundDrawable(R.color.colorPrimary)// 点击的背景。
+                    .setImage(android.R.drawable.ic_dialog_info) // 图标。
+                    .setText("删除") // 文字。
+                    .setTextColor(Color.BLUE) // 文字颜色。
+                    .setTextSize(16) // 文字大小。
+                    .setWidth(200) // 宽度。
+                    .setHeight(ViewGroup.LayoutParams.MATCH_PARENT); // 高度。
+            swipeRightMenu.addMenuItem(addItem2); // 添加一个按钮到左侧菜单。
+
+
+            // 上面的菜单哪边不要菜单就不要添加。
+        }
+    };
+
 }
--- app/src/main/java/com/mwkj/adapter/CollectionsAdapter.java
+++ app/src/main/java/com/mwkj/adapter/CollectionsAdapter.java
@@ -1,29 +1,204 @@
 package com.mwkj.adapter;
 
 import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.mwkj.activity.R;
 import com.mwkj.entity.ArtistInfoEntity;
-import com.qf.kenlibrary.base.AbsBaseAdapter;
+import com.mwkj.widget.GlideCircleTransform;
+import com.yanzhenjie.recyclerview.swipe.SwipeMenuAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by lenovo on 2016/11/1.
  *
  */
-public class CollectionsAdapter extends AbsBaseAdapter<ArtistInfoEntity.AlbumsBean> {
+public class CollectionsAdapter extends SwipeMenuAdapter<RecyclerView.ViewHolder> {
 
+
+    //定义RecyclerView item为空的布局类型
+    private static final int VIEW_TYPE = -1;
+
+    Context context;
+    List<ArtistInfoEntity.AlbumsBean> infolist;
+
+    private ArtInfoOnItemClickListener onItemClickListener;
+
+
+
+    public void setOnItemClickListener(ArtInfoOnItemClickListener onItemClickListener) {
+        this.onItemClickListener = onItemClickListener;
+    }
+
+    //构造方法
     public CollectionsAdapter(Context context) {
-        super(context, R.layout.item_artist_workinfo);
+        this.context = context;
+        this.infolist = new ArrayList<>();
+    }
+
+    //设置数据方法
+    public void setDatas(List<ArtistInfoEntity.AlbumsBean> infolist) {
+        this.infolist = infolist;
+        this.notifyDataSetChanged();
+    }
+
+
+    //SwipeMenuAdapter需要重写的2个方法
+    //移动item的方法
+    public void moveItem(int fromPosition,int toPosition){
+        ArtistInfoEntity.AlbumsBean remove = this.infolist.remove(fromPosition);
+        this.infolist.add(toPosition,remove);
+        this.notifyItemMoved(fromPosition,toPosition);
+    }
+
+    //移除item的方法
+    public void remove(int position){
+        this.infolist.remove(position);
+        this.notifyItemRemoved(position);
+    }
+
+    //SwipeMenuAdapter需要重写的2个方法
+    @Override
+    public View onCreateContentView(ViewGroup parent, int viewType) {
+        View view;
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        //RecyclerView item为空的适用类型
+        if (VIEW_TYPE == viewType) {
+            view = inflater.inflate(R.layout.empty_textview, parent, false);
+            return view;
+
+        }
+        //正常有数据的item布局,以及ViewHolder
+        view = inflater.inflate(R.layout.item_artist_workinfo, parent, false);
+        return view;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCompatCreateViewHolder(View realContentView, int viewType) {
+        if (VIEW_TYPE == viewType) {
+            return new MyEmptyHolder(realContentView);
+        }
+        return new ArtInfoHolder(realContentView);
+    }
+
+
+//    @Override
+//    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+//        View view;
+//        LayoutInflater inflater = LayoutInflater.from(context);
+//
+//        //RecyclerView item为空的适用类型
+//        if (VIEW_TYPE == viewType) {
+//            view = inflater.inflate(R.layout.empty_textview, parent, false);
+//
+//            //返回RecyclerView item为空的ViewHolder
+//            return new MyEmptyHolder(view);
+//        }
+//
+//        //正常有数据的item布局,以及ViewHolder
+//        view = inflater.inflate(R.layout.item_artist_workinfo, parent, false);
+//        return new ArtInfoHolder(view);
+//
+//    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+        //正常有item数据ViewHolder的实例
+        if (holder instanceof ArtInfoHolder) {
+            ((ArtInfoHolder)holder).art_info_title.setText(infolist.get(position).getAlbumName());
+            ((ArtInfoHolder)holder).art_info_name.setText(infolist.get(position).getArtistName());
+            ((ArtInfoHolder)holder).artist_capter.setText(infolist.get(position).getAlbumChapter() + "");
+            int playNumber = infolist.get(position).getPlayNumber();
+            if(playNumber/10000 != 0) {
+                ((ArtInfoHolder)holder).single_work_fans.setText((playNumber/10000) + "万");
+            }else {
+                ((ArtInfoHolder)holder).single_work_fans.setText(playNumber+"");
+            }
+
+            Glide.with(context)
+                    .load(infolist.get(position).getAlbumCover())
+                    .crossFade(500)
+                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+                    .placeholder(R.drawable.placeholder)
+                    .transform(new GlideCircleTransform(context))
+                    .thumbnail(0.1f)
+                    .into(((ArtInfoHolder)holder).artist_img);
+        }
     }
 
 
     @Override
-    public void bindView(ViewHolder viewHolder, ArtistInfoEntity.AlbumsBean data) {
-        viewHolder
-                .setTextView(R.id.art_info_title,data.getAlbumName())
-                .setTextView(R.id.art_info_name,data.getArtistName())
-                .setTextView(R.id.art_info_total_num,data.getAlbumChapter()+"")
-                .setTextView(R.id.artist_funs_num,data.getPlayNumber()+"")
-                .setImageView(R.id.art_info_img,data.getAlbumCover());
+    public int getItemCount() {
+//        return infolist.size() > 0 ? infolist.size() : 1;
+        return infolist.size() <= 0 ? 0 : infolist.size(); //三目运算符判断数据源集合的长度
     }
+
+    public int getItemViewType(int position) {
+        //若数据源集合的长度为0
+        if (infolist.size() <= 0) {
+            return VIEW_TYPE;
+        }
+        //正常数据源list集合返回类型
+        return super.getItemViewType(position);
+    }
+
+
+
+    //数据源集合的长度为0的ViewHolder
+    public class MyEmptyHolder extends RecyclerView.ViewHolder{
+
+        TextView textView;
+
+        public MyEmptyHolder(View itemView) {
+            super(itemView);
+            textView = (TextView) itemView.findViewById(R.id.tv_empty);
+        }
+    }
+
+
+    //正常item数据源的ViewHolder
+    public class ArtInfoHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        TextView art_info_title,art_info_name,artist_capter,single_work_fans;//
+
+        ImageView artist_img;
+        public ArtInfoHolder(View itemView) {
+            super(itemView);
+
+            this.art_info_title = (TextView) itemView.findViewById(R.id.art_info_title);
+            this.art_info_name = (TextView) itemView.findViewById(R.id.art_info_name);
+            this.artist_capter = (TextView) itemView.findViewById(R.id.art_info_total_num);
+            this.single_work_fans = (TextView) itemView.findViewById(R.id.artist_funs_num);
+            this.artist_img = (ImageView) itemView.findViewById(R.id.art_info_img);
+
+            itemView.setOnClickListener(this); //设置item的点击事件
+
+        }
+
+        //item的点击事件的监听
+        @Override
+        public void onClick(View v) {
+            if (onItemClickListener != null){
+                onItemClickListener.onItemClick(v,getAdapterPosition());
+            }
+        }
+    }
+
+
+    //item的点击事件的接口回调
+    public interface ArtInfoOnItemClickListener{
+        void onItemClick(View view, int position);
+    }
+
 }
